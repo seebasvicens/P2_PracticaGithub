799df49bd3c8b975981c06e471712a68ed13e6d0
seebasvicens <sebastia.vicens@godevel.com>
2022-11-24 15:39:00 +0100

    Creación del fichero README.md y introducción del fichero index.py

---
+++ README.md
@@ -0,0 +1,4 @@
+# Esta es la práctica relacionada con GitHub
+
+Autor: Sebastià Vicens Oliver
+Fecha: 24/11/2022
---
+++ index.py
@@ -0,0 +1,21 @@
+import random # Libreria necesaria para crear numeros randoms
+
+# Definimos el rango de los numeros aleatorios que se van a generar
+MIN_NUMBER = 0
+MAX_NUMBER = 1000
+
+random.seed(0)  # Fijamos la semilla a 0
+
+# Obtenemos los tres números aleatorios que
+# se encuentren entre MIN_NUMBER y MAX_NUMBER
+x = random.randint(MIN_NUMBER, MAX_NUMBER)
+y = random.randint(MIN_NUMBER, MAX_NUMBER)
+z = random.randint(MIN_NUMBER, MAX_NUMBER)
+
+suma = (x + y) + z
+
+# Mostramos el resultado de la suma por pantalla
+print(suma)
+
+# Al tener fijado una semilla veremos que el resultado siempre sera el mismo.
+# Para modificar este comportamiento tendriamos que eliminar la semilla
